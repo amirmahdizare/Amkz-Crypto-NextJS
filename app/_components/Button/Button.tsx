b4e1a097b3c4f9606e43b1fa11d393d97d6c53d9
Amir Mahdi <amirmahdikeshanzare@gmail.com>
2025-02-27 15:58:53 +0330

    Modify Button

--- app/_components/Button/Button.tsx
+++ app/_components/Button/Button.tsx
@@ -3,7 +3,7 @@ import { ButtonProps } from './props'
 
 export const Button = ({ children, ...props }: ButtonProps) => {
     return (
-        <button className='bg-primary rounded-xl px-6 py-2 text-white' {...props}>
+        <button className='bg-primary rounded-lg px-6 py-3 text-white' {...props}>
             {children}
         </button>
     )

187d86c692c51f7076da340db500bdd024df055b
Amir Mahdi <amirmahdikeshanzare@gmail.com>
2025-02-27 15:47:11 +0330

    Develope Not Found Page

---
+++ app/not-found.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from './_components'
+import Link from 'next/link'
+
+export default function () {
+    return (
+        <div className='flex flex-col h-full min-h-96 gap-4 items-center justify-center w-full'>
+            <h1 className='text-gray-200 text-2xl '>Page Doesn't Exist!</h1>
+            <Link href={'/'}>
+                <Button>Back To Home</Button>
+            </Link>
+        </div>
+    )
+}

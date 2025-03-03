4e13927f95022e1f4959177efc5c93da21752e26
Amir Mahdi <amirmahdikeshanzare@gmail.com>
2025-03-03 19:26:57 +0330

    Check Functionality

--- app/_components/Header/components/Menu.tsx
+++ app/_components/Header/components/Menu.tsx
@@ -11,9 +11,9 @@ export const Menu = () => {
     const [open, setOpen] = useState<boolean>(false)
 
     useEffect(() => {
-        if(typeof document!='object') return 
+        if (typeof document != 'object') return
 
-        
+
         if (open)
             document.body.style.overflow = 'hidden'
         else
@@ -21,37 +21,41 @@ export const Menu = () => {
 
     }, [open])
 
-    return (
-        <>
-            <div className='cursor-pointer text-white lg:hidden' onClick={() => setOpen(true)}>
-                <Bars3Icon className='size-7' />
-            </div>
+    if (typeof document != 'undefined')
 
-
-            <div className='hidden lg:flex order-2'>
-                <Navbar />
-            </div>
-
-            {createPortal(<div className={clsx('flex flex-col gap-2 items-center fixed top-0 right-0 w-full bg-white h-full transition-all duration-700 z-10', open ? 'translate-x-0 opacity-100' : '-translate-x-full opacity-0')}>
-
-                <div className='flex flex-col gap-4 justify-start items-start w-full p-4'>
-
-                    <div className=' flex flex-row-reverse justify-between w-full' >
-                        <XMarkIcon className='text-black size-7 cursor-pointer' onClick={() => setOpen(false)} />
-                        <Logo mode='light' />
-                    </div>
-
-                    <div className='bg-gray-200 w-full h-[1px]'></div>
-
-                    <Navbar />
-
+        return (
+            <>
+                <div className='cursor-pointer text-white lg:hidden' onClick={() => setOpen(true)}>
+                    <Bars3Icon className='size-7' />
                 </div>
 
-            </div>, document.body)}
+
+                <div className='hidden lg:flex order-2'>
+                    <Navbar />
+                </div>
+
+                {createPortal(<div className={clsx('flex flex-col gap-2 items-center fixed top-0 right-0 w-full bg-white h-full transition-all duration-700 z-10', open ? 'translate-x-0 opacity-100' : '-translate-x-full opacity-0')}>
+
+                    <div className='flex flex-col gap-4 justify-start items-start w-full p-4'>
+
+                        <div className=' flex flex-row-reverse justify-between w-full' >
+                            <XMarkIcon className='text-black size-7 cursor-pointer' onClick={() => setOpen(false)} />
+                            <Logo mode='light' />
+                        </div>
+
+                        <div className='bg-gray-200 w-full h-[1px]'></div>
+
+                        <Navbar />
+
+                    </div>
+
+                </div>, document.body)}
 
 
 
 
-        </>
-    )
+            </>
+        )
+
+    return <></>
 }
